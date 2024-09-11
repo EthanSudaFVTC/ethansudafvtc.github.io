8bdff9e5857e607fff1752f02249fa5e68aa7804
Ethan Suda <300066223@fvtc.edu>
2024-09-11 13:21:17 -0500

    Added if/else if statement to change message based on time of day

--- site.js
+++ site.js
@@ -1,9 +1,23 @@
+console.log('Test') // Tests to see if site.js is linked correctly
+
 const hours = new Date().getHours() // get the current hour
 
 const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 
-const welcomeMessage = document.getElementById('welcome')
+const welcomeMessage = document.getElementById('welcome') // puts welcome div in variable
 
-welcomeMessage.textContent = "Hello World"
+// checks times to add correct message
+if (isMorning)
+{
+    welcomeMessage.textContent = "Good Morning"
+}
+else if (isAfternoon)
+{
+        welcomeMessage.textContent = "Good Afternoon"
+}
+else if (isEvening)
+{
+    welcomeMessage.textContent = "Good Evening"
+}
