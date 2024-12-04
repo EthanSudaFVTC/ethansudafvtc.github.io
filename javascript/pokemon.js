58337c1202c13e15150d657296d09d603f5d2471
Ethan Suda <4ethans@gmail.com>
2024-12-04 16:15:10 -0600

    attempting to create program to display random pokemon. not currently functioning, but most code is there.

--- javascript/pokemon.js
+++ javascript/pokemon.js
@@ -1,1 +1,29 @@
-console.log("hello world")
+
+// // code to add image to the page
+// const img = document.createElement('img')
+// img.src = // url of the image from the 'front_default' property
+// img.alt = // name of the pokemon
+// parentElement.append(img)
+
+// // code to get a random pokemon
+// const url = 'https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random() * 150)
+
+
+const getRandomPokemon = async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random() * 150)
+    const response = await fetch(url)
+    const randomPokemon = await response.json()
+    return randomPokemon
+}
+
+const renderPokemon = (randomPokemon) => {
+    const parentElement = document.getElementById('random-pokemon')
+    const img = document.createElement('img')
+    img.src = randomPokemon.sprites.front_default
+    parentElement.append(img)
+}
+
+(async () => {
+    const pokemon = await getRandomPokemon()
+    renderPokemon(pokemon)
+})
